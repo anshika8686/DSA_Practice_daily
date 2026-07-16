54e4cd80a844d2e39714f2a9f475a64d63cf77b0
Anshika Goyal <goyalanshika544@gmail.com>
2026-07-16 11:25:16 +0530

    Time: 1 ms (76.8%), Space: 15 MB (12.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
-    {
-        //TC=O(N2) SC=O(1)
-        vector<int>index;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    index.push_back(i);
-                    index.push_back(j);
-                }
-            }
-        }
-        return index;  
-    }
-
     // vector<int> twoSum(vector<int>& nums, int target) 
     // {
-    //     //TC=O(Nlogn) SC=O(1)
+    //     //TC=O(N2) SC=O(1)
     //     vector<int>index;
     //     int n=nums.size();
-    //     sort(nums.begin(),nums.end());
-    //     int l=0,r=n-1;
-    //     while(l<r){
-    //         if(nums[l]+nums[r]==target){
-    //             index.push_back(l);
-    //             index.push_back(r);
-    //             break;
-    //         }
-    //         else if(nums[l]+nums[r]>target){
-    //             r--;
-    //         }
-    //         else{
-    //             l++;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i+1;j<n;j++){
+    //             if(nums[i]+nums[j]==target){
+    //                 index.push_back(i);
+    //                 index.push_back(j);
+    //             }
     //         }
     //     }
     //     return index;  
     // }
+
+     vector<int> twoSum(vector<int>& nums, int target) 
+     {
+        unordered_map<int,int>mpp;
+        int complement;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            complement=target-nums[i];
+        if(mpp.find(complement)!=mpp.end()){
+            return {mpp[complement], i};
+        }
+        mpp[nums[i]]=i;
+     }
+     return {};
+    }
+
+    
 };
