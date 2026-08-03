0b3cf9a4890256394ac3710b70d03fb5e461719d
Anshika Goyal <goyalanshika544@gmail.com>
2026-08-03 12:39:55 +0530

    Time: 29 ms (41.31%), Space: 24.2 MB (20.09%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid)
+    {
+        //islands->connected components
+        int count=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                if(grid[row][col]=='1' && !vis[row][col]){
+                    count++;
+                    bfs(row,col,vis,grid,n,m);
+                }
+
+            }
+        }
+        return count;
+        
+    }
+    void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>& grid,int n,int m){
+        vis[row][col]=1;
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+
+            int delrow[4]={0,-1,0,+1};
+            int delcol[4]={-1,0,+1,0};
+            for(int k=0;k<4;k++){
+                int nrow=row+delrow[k];
+                int ncol=col+delcol[k];
+
+                if(nrow>=0 && nrow<n && 
+                ncol>=0 && ncol<m && 
+                grid[nrow][ncol]=='1' &&
+                !vis[nrow][ncol]){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+
+    }
+
+    // void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>& grid,,int n,int m)
+    // {
+    //     vis[row][col]=1;
+    //     int delrow=[0,-1,0,+1];
+    //     int delcol=[-1,0,+1,0];
+    //     for(int k=0;k<4;k++){
+    //             int nrow=row+delrow[k];
+    //             int ncol=col+delcol[k];
+
+    //          if(nrow>=0 && nrow<n && 
+    //             ncol>=0 && ncol<m && 
+    //             grid[nrow][ncol]=='1' &&
+    //             !vis[nrow][ncol])
+    //             {
+    //                 vis[nrow][ncol]=1;
+    //                 dfs({nrow,ncol,vis,grid,n,m});
+    //             }
+    //         }
+    //     }
+};
