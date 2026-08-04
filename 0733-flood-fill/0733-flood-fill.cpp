7fe352e8efe1f30a8b2172a43a4388b278dc3224
Anshika Goyal <goyalanshika544@gmail.com>
2026-08-04 16:37:30 +0530

    Time: 0 ms (100%), Space: 18 MB (75.77%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,13 +3,13 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
         int m=image[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        // vector<vector<int>>vis(n,vector<int>(m,0));
         int orgColor=image[sr][sc];
-        dfs(image,sr,sc,color,orgColor,vis,n,m);
+        if(orgColor==color) return image;
+        dfs(image,sr,sc,color,orgColor,n,m);
         return image; 
     }
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color,int orgColor,vector<vector<int>>&vis,int n,int m){
-        vis[sr][sc]=1;
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color,int orgColor,int n,int m){
         image[sr][sc]=color;
         int delrow[4]={0,-1,0,+1};
         int delcol[4]={-1,0,+1,0};
@@ -19,9 +19,8 @@ public:
 
             if(nrow>=0 && nrow<n &&
             ncol>=0 && ncol<m &&
-            !vis[nrow][ncol] && 
             (image[nrow][ncol]==orgColor)){
-                dfs(image,nrow,ncol,color,orgColor,vis,n,m);
+                dfs(image,nrow,ncol,color,orgColor,n,m);
             }
             
         }
