52a096be2e537d3da946670be320cca741e75f94
Anshika Goyal <goyalanshika544@gmail.com>
2026-08-19 11:10:14 +0530

    Time: 8 ms (61.11%), Space: 33.4 MB (55.56%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -6,55 +6,53 @@ int delcol[4]={-1,0,+1,0};
     {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
         int count=0;
 
         
         for(int i=0;i<m;i++){
             //first row
             if(grid[0][i]==1){
-                vis[0][i]=1;
-                dfshelper(0,i,vis,grid,n,m);
+                grid[0][i]=0;
+                dfshelper(0,i,grid,n,m);
             }
             //last row
             if(grid[n-1][i]==1){
-                vis[n-1][i]=1;
-                dfshelper(n-1,i,vis,grid,n,m);
+                grid[n-1][i]=0;
+                dfshelper(n-1,i,grid,n,m);
             }
         }
             for(int i=0;i<n;i++){
             //first col
                 if(grid[i][0]==1){
-                vis[i][0]=1;
-                dfshelper(i,0,vis,grid,n,m);
+                grid[i][0]=0;
+                dfshelper(i,0,grid,n,m);
             }
             //last col
             if(grid[i][m-1]==1){
-                vis[i][m-1]=1;
-                dfshelper(i,m-1,vis,grid,n,m);
+                grid[i][m-1]=0;
+                dfshelper(i,m-1,grid,n,m);
             }
         }
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(grid[i][j]==1 && !vis[i][j]){
+            if(grid[i][j]==1){
                 count++;
             }
         }
     }
     return count;
 }
-    void dfshelper(int row,int col,vector<vector<int>>&vis,vector<vector<int>>& grid,int n,int m){
+    void dfshelper(int row,int col,vector<vector<int>>& grid,int n,int m){
         for(int i=0;i<4;i++){
             int nrow=delrow[i]+row;
             int ncol=delcol[i]+col;
 
             if(nrow>=0 && nrow<n && 
             ncol>=0 && ncol<m &&
-             grid[nrow][ncol]==1 && 
-             !vis[nrow][ncol])
+             grid[nrow][ncol]==1)
              {
-                vis[nrow][ncol]=1;
-                dfshelper(nrow,ncol,vis,grid,n,m);
+                grid[nrow][ncol]=0;
+                dfshelper(nrow,ncol,grid,n,m);
             }
         }
     }
