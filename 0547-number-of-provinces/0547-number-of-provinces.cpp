3bb1847424d9479c9faeeec304b0082886f08ebb
Anshika Goyal <goyalanshika544@gmail.com>
2026-09-20 13:00:25 +0530

    Time: 0 ms (100%), Space: 19.6 MB (58.97%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-
-    void dfs(int node,vector<vector<int>>& isConnected,vector<int>&vis){
-        vis[node]=1;
-        for(int u=0;u<isConnected.size();u++){
-            if(!vis[u] && isConnected[node][u] == 1 ){
-                dfs(u,isConnected,vis);
-            }
-        }
+    int findPar(int u,vector<int>&parent){
+        if(u==parent[u]) return u;
+        return parent[u]=findPar(parent[u],parent);
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int cnt=0;
-        int V=isConnected.size();  
-        vector<int>vis(V,0);
+        int V=isConnected.size();
+        vector<int>parent(V+1);
+        vector<int>rank(V+1,0);
+        unordered_set<int>st;
+        for(int i=0;i<V;i++){
+            parent[i]=i;
+        }
+        int count=1;
+        for(int i=0;i<V;i++){
+            for(int j=i+1;j<V;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    int pu=findPar(i,parent);
+                    int pv=findPar(j,parent);
 
-        for(int u=0;u<V;u++){
-            if(!vis[u]){
-                cnt++;
-                dfs(u,isConnected,vis);
+                    if(pu == pv) continue;
+
+                    if(rank[pu]<rank[pv]) parent[pu]=pv;
+                    else if(rank[pu]>rank[pv]) parent[pv]=pu;
+                    else {
+                        parent[pu]=pv;
+                        rank[pv]++;
                     }
-                }    
-        return cnt;
+                }
+            }
+        }
+        for(int i=0;i<V;i++){
+            st.insert(findPar(i,parent));
+        }
+        return st.size();
+
         
     }
 };
