f5e7b5762df624d073e0a53077c39b62e77e0ec4
Anshika Goyal <goyalanshika544@gmail.com>
2026-09-21 15:12:09 +0530

    Time: 25 ms (31.54%), Space: 46.7 MB (50.32%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int findPar(int u,vector<int>&parent){
+        if(u==parent[u]) return u;
+        return parent[u]=findPar(parent[u],parent);
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        vector<int>rank(n+1,0);
+        vector<int>parent(n+1);
+        int components=n;
+        int count=0;
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        for(auto connection:connections){
+            int u=connection[0];
+            int v=connection[1];
+
+            int pu=findPar(u,parent);
+            int pv=findPar(v,parent);
+
+            if(pu==pv) {
+                count++;
+                continue;
+            }
+            if(rank[pu]<rank[pv])  parent[pu]=pv;
+            else if(rank[pu]>rank[pv]) parent[pv]=pu;
+            else{
+                parent[pu]=pv;
+                rank[pv]++;
+            }
+            components--;
+        }
+       if(count>=components-1) return components-1;
+       return -1;
+    }
+};
