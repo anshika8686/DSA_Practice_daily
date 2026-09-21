049dfe22f16dfe9da85bac285e5537e5b867f20c
Anshika Goyal <goyalanshika544@gmail.com>
2026-09-21 15:15:21 +0530

    Time: 4 ms (80.19%), Space: 42.1 MB (82.09%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -5,6 +5,7 @@ public:
         return parent[u]=findPar(parent[u],parent);
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size()<n-1) return -1;
         vector<int>rank(n+1,0);
         vector<int>parent(n+1);
         int components=n;
@@ -12,7 +13,7 @@ public:
         for(int i=0;i<n;i++){
             parent[i]=i;
         }
-        for(auto connection:connections){
+        for(const auto&connection:connections){
             int u=connection[0];
             int v=connection[1];
 
